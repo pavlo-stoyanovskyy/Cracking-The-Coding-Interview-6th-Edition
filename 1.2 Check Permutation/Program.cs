47bd5d8966f6289af3fe1c004221dd5e84f93ac6
pavel.stoyanovskyy <pavel.stoyanovskyy@leapfrogbuilders.com>
2018-08-13 20:49:25 -0400

    1.2 Check Permutation

---
+++ 1.2 Check Permutation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._2_Check_Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = Permutation("apple", "papel");
+        }
+
+        static bool Permutation(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            var d = new Dictionary<char, int>();
+
+            foreach (var c in s)
+            {
+                if (d.ContainsKey(c))
+                    d[c]++;
+                else
+                    d[c] = 1;
+            }
+
+            foreach (var c in t)
+            {
+                d[c]--;
+                if (d[c] < 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
